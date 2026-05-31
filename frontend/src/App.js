6b06d57e61a363deada295d11aee83349d19c1bf
Karthik <karthikraj1597@gmail.com>
2026-05-31 22:15:08 +0800

    fix app scroll

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,8 @@
 import React from "react";
 import Home from "./pages/Home";
-import PopularDestinations from "./components/PopularDestinations";
-
 
 function App() {
-  return (
-    <div className="app-scroll">
-      <Home />
-      <PopularDestinations />
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
