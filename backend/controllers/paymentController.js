e43a3282eb9e5406c439fec2bc4028775e542818
Karthik <karthikraj1597@gmail.com>
2026-06-03 12:49:08 +0800

    update cors

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,9 +1,7 @@
 import Stripe from "stripe";
-import Booking from "../models/Booking.js";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-// CREATE PAYMENT
 export const createCheckoutSession = async (req, res) => {
   try {
     const { hotelName, price, checkIn, checkOut, guests } = req.body;
@@ -25,24 +23,14 @@ export const createCheckoutSession = async (req, res) => {
         },
       ],
 
-      success_url: `${process.env.CLIENT_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${process.env.CLIENT_URL}/success`,
       cancel_url: `${process.env.CLIENT_URL}/booking`,
     });
 
-    // TEMP STORE DATA (IMPORTANT)
-    // we will save after success later
-    global.bookingTemp = {
-      hotelName,
-      price,
-      checkIn,
-      checkOut,
-      guests
-    };
+    return res.json({ url: session.url });
 
-    res.json({ url: session.url });
-
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Stripe error" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
   }
 };
