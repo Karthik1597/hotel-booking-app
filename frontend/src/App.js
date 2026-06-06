96a307d00ddca83d62dc9568e2441f15b532f717
Karthik <karthikraj1597@gmail.com>
2026-06-06 11:55:24 +0800

    fix App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import Home from "./pages/Home";
 import HotelList from "./pages/HotelList";
 import HotelDetails from "./pages/HotelDetails";
@@ -10,30 +13,26 @@ import BookingSuccess from "./pages/BookingSuccess";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
+      {/* ✅ ADD THIS ONCE */}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+      />
 
+      <Routes>
         <Route path="/" element={<Home />} />
 
-        <Route
-          path="/hotels/:city"
-          element={<HotelList />}
-        />
+        <Route path="/hotels/:city" element={<HotelList />} />
 
-        <Route
-          path="/hotel-details"
-          element={<HotelDetails />}
-        />
+        <Route path="/hotel-details" element={<HotelDetails />} />
 
-        <Route
-          path="/booking"
-          element={<BookingPage />}
-        />
+        <Route path="/booking" element={<BookingPage />} />
 
-        <Route
-          path="/success"
-          element={<BookingSuccess />}
-        />
-
+        <Route path="/success" element={<BookingSuccess />} />
       </Routes>
     </BrowserRouter>
   );
