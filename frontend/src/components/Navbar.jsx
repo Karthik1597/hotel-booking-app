d837c0e3025f422c892d4b1212fd1676098ec7c8
Karthik <karthikraj1597@gmail.com>
2026-06-07 15:50:18 +0800

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,12 +10,6 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="logo">QuickStay</div>
 
-        <ul className="nav-links">
-          <li>Home</li>
-          <li>Hotels</li>
-          <li>Experience</li>
-          <li>About</li>
-        </ul>
 
         <button
           className="login-btn"
